d27c17aaba4dd2dbf5741bc49c44b55a34af2e63
crisaac2709 <crisaac2002@gmail.com>
2025-06-26 17:33:02 -0500

    Pequeños cambios realizados

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -103,8 +103,12 @@ REST_FRAMEWORK = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'MaizVision',
+        'USER' : 'postgres',
+        'PASSWORD' : 'admin',
+        'HOST' : 'localhost',
+        'PORT' : '5433'
     }
 }
 
